734baf371d618d966eae7d2878a5af280ccff892
glebtv <glebtv@gmail.com>
2013-12-30 14:03:07 +0400

    fix rails admin deletion

--- lib/mongoid_rating/rate.rb
+++ lib/mongoid_rating/rate.rb
@@ -8,6 +8,10 @@ module Mongoid::Rating
 
     # rate can be integer or float so we don't force class here
     field :value
+
+    def rails_admin_default_object_label_method
+      "Mongoid::Rating::Rate ##{id.to_s}"
+    end
   end
 end
 
--- lib/mongoid_rating/version.rb
+++ lib/mongoid_rating/version.rb
@@ -1,3 +1,3 @@
 module MongoidRating
-  VERSION = "0.1.4"
+  VERSION = "0.1.5"
 end
